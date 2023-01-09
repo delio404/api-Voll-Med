07ad3e7e448b05a2a935eb8684087c35c4115bf5
Adelio Aguirre de Castro <adelio.castro@pti.org.br>
2023-01-09 17:41:22 -0300

    Producao de dados na API

--- src/main/java/com/pti/api/controller/MedicoController.java
+++ src/main/java/com/pti/api/controller/MedicoController.java
@@ -1,15 +1,15 @@
 package com.pti.api.controller;
 
 import com.pti.api.medico.DadosCadastroMedico;
+import com.pti.api.medico.DadosListagemMedico;
 import com.pti.api.medico.Medico;
 import com.pti.api.repository.MedicoRepository;
 import jakarta.transaction.Transactional;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/medicos")
@@ -23,5 +23,9 @@ public class MedicoController {
     public void cadastrar(@RequestBody @Valid DadosCadastroMedico dados){
        repository.save(new Medico(dados));
 
+    }
+    @GetMapping
+    public List<DadosListagemMedico> listar(){
+        return repository.findAll().stream().map(DadosListagemMedico::new).toList();
     }
 }
